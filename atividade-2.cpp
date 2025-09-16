fb171f073713ae850d96774dd5d89e4c33dad351
Fernando Sergio <fernandosergio2196@gmail.com>
2025-09-16 16:07:34 -0300

    Atividade 2 - Remover Lista
    2. Crie um método que remove o segundo elemento da lista. Se a lista tiver tamanho <= 1, seu método não deve fazer nada.

---
+++ atividade-2.cpp
@@ -0,0 +1,165 @@
+#include<stdio.h>
+
+struct Node {
+
+    int value;
+    Node *next;
+
+    Node(int _value) {
+        value = _value;
+        next = NULL;
+    }
+
+    Node() {
+        next = NULL;
+    }
+
+};
+
+struct List {
+
+    Node *start;
+    Node *end;
+    int c;
+
+    List() {
+        start = NULL;
+        end = NULL;
+        c = 0;
+    }
+
+    bool empty() {
+        return start == NULL;
+    }
+
+    void pushBack(int value) {
+
+        Node *n = new Node(value);
+        c++;
+
+        if (empty()) {
+            start = n;
+            end = n;
+            return;
+        }
+
+        end->next = n;
+        end = n;
+
+    }
+
+    void pushFront(int value) {
+
+        Node *n = new Node(value);
+        c++;
+
+        if (empty()) {
+            start = n;
+            end = n;
+            return;
+        }
+
+        n->next = start;
+        start = n;
+
+    }
+
+    void print() {
+        //printf("start: %d end: %d\n", start, end);
+        Node *aux = start;
+        while (aux != NULL) {
+            //printf("[%d] %d [%d] -> ", aux, aux->value, aux->next);
+            printf("%d -> ", aux->value);
+            aux = aux->next;
+        }
+        printf("\n");
+    }
+
+    // complexidade tempo: O(n)
+    /*int size() {
+        int c = 0;
+        Node *aux = start;
+        while (aux != NULL) {
+            c++;
+            aux = aux->next;
+        }
+        return c;
+    }*/
+
+    // O(1)
+    int size() {
+        return c;
+    }
+
+    // O(1)
+    void popFront() {
+
+        if (empty()) return;
+
+        c--;
+
+        if (start == end) {
+            delete(start);
+            start = NULL;
+            end = NULL;
+            return;
+        }
+
+        Node *aux = start;
+        start = start->next;
+        delete(aux);
+
+    }
+
+    // O(n)
+    void popBack() {
+
+        if (empty()) return;
+
+        c--;
+
+        if (start == end) {
+            delete(start);
+            start = NULL;
+            end = NULL;
+            return;
+        }
+
+        Node *newEnd = start;
+        while (newEnd->next != end) {
+            newEnd = newEnd->next;
+        }
+
+        delete(end);
+        end = newEnd;
+        end->next = NULL;
+
+    }
+    
+    void atv2(){
+        Node *aux;
+        if(empty()){
+            printf("Vazia a lista");
+        }else if(size() <= 1){
+            return;
+        }else{
+            aux = start->next->next;
+            delete(start->next);
+            start->next = aux;
+        }
+    }
+
+};
+
+int main() {
+    List l;
+    l.pushBack(10);
+    l.pushBack(3);
+    l.pushBack(5);
+    l.pushBack(8);
+    l.atv2();
+    l.print();
+    l.atv2();
+    l.print();
+    return 0;
+}
